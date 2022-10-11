c2b341157b0c3c394af9b4d6963fa7ef3bfb7372
Eric Coimbra <eric.coimbra@gmail.com>
2022-10-11 11:26:20 +0100

    iteration2 done

--- src/chronometer.js
+++ src/chronometer.js
@@ -5,30 +5,38 @@ class Chronometer {
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    if (printTimeCallback) {
+      setInterval(() =>{
+        printTimeCallback()
+      },1000)
+    }
+    setInterval(()=>{
+      this.currentTime += 1;
+    }, 1000)
+
   }
 
   getMinutes() {
-    // ... your code goes here
+    
   }
 
   getSeconds() {
-    // ... your code goes here
+    
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    
   }
 
   stop() {
-    // ... your code goes here
+    
   }
 
   reset() {
-    // ... your code goes here
+    
   }
 
   split() {
-    // ... your code goes here
+    
   }
 }
